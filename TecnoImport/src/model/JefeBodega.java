0057c4e482321c5fdfee3b0773b44e7ab29bab09
Ana Carrion Barboto <anacbarboto@atlassian.com>
2019-08-15 19:46:51 -0500

    Clase JefeBodega: Nuevos metodos agregados

--- TecnoImport/src/model/JefeBodega.java
+++ TecnoImport/src/model/JefeBodega.java
@@ -10,12 +10,21 @@ package model;
  * @author scmz2607
  */
 public class JefeBodega extends Empleado{
-//    protected String numCedula, nombre, cargo, apellido, direccion, numTelefono;
-//    protected Usuario usuario;
 
     public JefeBodega(String numCedula, String nombre, String cargo, String apellido, String direccion, String numTelefono) {
         super(numCedula, nombre, cargo, apellido, direccion, numTelefono);
     }
     
+    public void crearRutaEntrega(){
+        System.out.println("Clase JefeBodega Metodo crearRutaEntrega");
+    }
+    
+    public void asignarRepartidorDisponible(){
+        System.out.println("Clase JefeBodega Metodo asignarRepartidorDisponible");
+    }
+    
+    public void actualizarEnvios(){
+        System.out.println("Clase JefeBodega Metodo actualizarEnvios");
+    }
     
 }
