d054ce7f42ed8484838851ad4ad1b68c7fac35ab
Nicole Alvarez <ngalvare@hotmai.com>
2019-08-15 20:33:09 -0500

    Se agrego Envios

---
+++ TecnoImport/src/model/EnvioAbastecimiento.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Nicole Alvarez
+ */
+public class EnvioAbastecimiento extends Envio {
+    private Local lugarDestino;
+
+
+    public EnvioAbastecimiento(int id, Date fechaEmision, EstadoEnvio estado, Empleado originador, Bodega Bodega, Local local,Empleado originario) {
+        super(id, fechaEmision, estado, originador, Bodega);
+        this.lugarDestino = local;
+        
+    }
+    
+    
+    
+}
