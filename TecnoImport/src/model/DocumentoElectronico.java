0d7762999a57d1ed3f52523d39eebcb02c74d5b5
Ana Carrion Barboto <anacbarboto@atlassian.com>
2019-08-15 19:26:24 -0500

    Clase DocumentoElectronico

---
+++ TecnoImport/src/model/DocumentoElectronico.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author ANA
+ */
+public class DocumentoElectronico {
+    
+    private int id;
+    //private Pago tipoPago
+    private Cliente cliente;
+    private Date fecha;
+    private String detalle;
+    
+}
