1b9d047817247a5b04c70aacd4393da12042df69
Ana Carrion Barboto <anacbarboto@atlassian.com>
2019-08-15 19:35:41 -0500

    Clase Factura

---
+++ TecnoImport/src/model/Factura.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.List;
+
+/**
+ *
+ * @author ANA
+ */
+public class Factura extends DocumentoElectronico{
+    
+    private List<Producto> productos;
+
+    public Factura(List<Producto> productos) {
+        this.productos = productos;
+    }
+
+    public List<Producto> getProductos() {
+        return productos;
+    }
+
+    public void setProductos(List<Producto> productos) {
+        this.productos = productos;
+    }
+    
+    
+}
